79def69807fc9f820b917549e2827ae19eaa1b8f
je3f0o <je3f0o@gmail.com>
2020-06-20 18:22:26 +0800

    Floating numbers

--- main.c
+++ main.c
@@ -12,6 +12,9 @@
  * - int   4 byte
  * - long  8 byte
  *
+ * Floating point numbers
+ * - float
+ * - double
  *
  * User Defined datatypes
  * ======================
@@ -22,25 +25,13 @@
  * sizeof
  */
 
-// Max Integer Computer Science
-// https://en.wikipedia.org/wiki/Integer_(computer_science)
-
-/*
-             -8 ..-3 -2 -1   1 2 3 4 5.. 7
----------------------------0-----------------------------
-*/
-
 int main ()
 {
-    unsigned char char_num   = 255;
-    unsigned short short_num = 5000;
-    unsigned int int_num     = 1000000;
-    unsigned long long_num   = 1e7;
+    float  fPI = 3.14159265359;
+    double dPI = 3.14159265359;
 
-    printf("Char value: %d size: %ld bytes.\n"  , char_num  , sizeof char_num);
-    printf("Short value: %d size: %ld bytes.\n" , short_num , sizeof short_num);
-    printf("Int value: %d size: %ld bytes.\n"   , int_num   , sizeof int_num);
-    printf("Long value: %ld size: %ld bytes.\n" , long_num  , sizeof long_num);
+    printf("Float  PI: %f, size: %ld bytes.\n", fPI, sizeof fPI);
+    printf("Double PI: %f, size: %ld bytes.\n", dPI, sizeof dPI);
 
     return 0;
 }
